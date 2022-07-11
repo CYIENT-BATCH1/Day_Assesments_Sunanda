617fe85c916f1954dc3a84eca7a0d6e68198a923
Sunanda-Cyient <108972481+Sunanda-Cyient@users.noreply.github.com>
2022-07-11 17:52:14 +0530

    Update DAY_13_2.c

--- DAY_13/DAY_13_2.c
+++ DAY_13/DAY_13_2.c
@@ -8,6 +8,9 @@ int main(){
             scanf("%x",&Sensor_Values[i][j]);
          }
      }
+    //Check condition for corrupted data and print that time stamp at which it occurs
+    printf("\n");
+    printf("******************Time stamp of corrupted data************************\n");
     for(i=0;i<200;i+=10){
         for(j=0;j<1;j++){
             for(k=0;k<3;k++);
@@ -17,7 +20,9 @@ int main(){
               printf("valid sensor data %d\n",i);
             }
          }
-         
+     //check the condition for non-corrupted data and print the time stamp at which it occurs
+    printf("\n");
+    printf("******************Valid data************************\n");
     for(i=0;i<200;i+=10){
         for(j=0;j<1;j++){
             if(Sensor_Values[i][j]==0x00||Sensor_Values[i][j]==0xFF)
